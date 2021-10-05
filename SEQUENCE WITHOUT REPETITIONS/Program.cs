e1b4df9624dad04c83b095934a583992614e1e1b
lairon27 <74205672+lairon27@users.noreply.github.com>
2021-10-05 19:44:31 +0300

    Add files via upload

---
+++ SEQUENCE WITHOUT REPETITIONS/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+// There is a sequence of numbers. Print all its items without duplicates.
+//     Input:
+//
+// 1. An array of positive integers.
+//
+// 2. Termination number -1
+
+using System.Linq;
+
+namespace SEQUENCE_WITHOUT_REPETITIONS
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                int j;
+                for (j = 0; j < i; j++)
+                    if (numbers[i] == numbers[j])
+                        break;
+
+                if (numbers[i] == -1)
+                {
+                    break;
+                }
+
+                if (i == j)
+                    Console.Write(numbers[i] + " ");
+            }
+            
+            Console.Write(-1);
+        }
+    }
+}
---
+++ SEQUENCE WITHOUT REPETITIONS/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SEQUENCE_WITHOUT_REPETITIONS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SEQUENCE_WITHOUT_REPETITIONS")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("22BFD6DB-4571-405A-BA49-96CC5AFC82A0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
