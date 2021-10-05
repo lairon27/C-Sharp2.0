e847053ef9e82024b6d2b693c04690cfc058aeb6
lairon27 <74205672+lairon27@users.noreply.github.com>
2021-10-05 19:48:13 +0300

    Add files via upload

---
+++ CIRCLE AREA/Program.cs
@@ -0,0 +1,17 @@
+﻿// There is a length of a circle. Find the area of the circle.
+
+using System;
+
+namespace CIRCLE_AREA
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var length = float.Parse(Console.ReadLine());
+            var r = length / (2 * Math.PI);
+
+            Console.Write(Math.Round((Math.PI * r * r), 9));
+        }
+    }
+}
